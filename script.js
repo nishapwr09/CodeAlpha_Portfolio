83a8d52ce9e3e608067ce182a73edf09c030ce32
nishapwr09 <nishapwr5830@gmail.com>
2026-07-27 00:16:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const display = document.getElementById('display');
+
+
+function appendValue(input) {
+    if (display.value === '0') {
+        display.value = input;
+    } else {
+        display.value += input;
+    }
+}
+
+
+function clearDisplay() {
+    display.value = '';
+}
+
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+
+function calculate() {
+    try {
+       
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
